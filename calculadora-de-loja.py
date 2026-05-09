62fceb789b6caf855fb4fab5a2e83fae91ff14bf
Edu-Elbis-080 <edu.gabrielpin@gmail.com>
2026-05-08 23:04:08 -0300

    primeiro dever

---
+++ calculadora-de-loja.py
@@ -0,0 +1,43 @@
+#calculadora de loja
+
+print("Bem-vindo à calculadora de loja!")
+nome_produto = "trufa_de_chocolate"
+preço_do_produto = 5.00
+quantidade_comprada = 4
+
+valor_total = preço_do_produto * quantidade_comprada
+valor_com_desconto = valor_total * 0.10 
+
+print("quantia:" + str(quantidade_comprada))
+print("preço do produto:" + str(preço_do_produto))
+print("valor total:" + str(valor_total))
+print("valor com desconto de:" + str(valor_com_desconto))
+print("valor final:" + str(valor_total - valor_com_desconto))
+
+import time
+import sys
+
+def digitar(texto):
+    for char in texto:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+senha = 1538
+input(print("insira a senha para finalizar a compra: "))
+if print(digite a senha) senha == 1538:
+    print("compra finalizada com sucesso!")
+else:    print("senha incorreta, tente novamente.")
+
+input(print("vamos tentar denovo, certo: y/n "))
+resposta = input()
+
+if resposta == "y":
+    senha = 1538
+    input(print("insira a senha para finalizar a compra: "))
+
+else:    print("compra cancelada, obrigado por visitar nossa loja!")
+
+if senha == 1538:
+    print("tente novamente!")
+else:    print("senha incorreta, tente novamente.")
