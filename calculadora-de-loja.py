f8c1fcf2bc80202c7f8d2c24b3448985f38cd1ba
Edu-Elbis-080 <edu.gabrielpin@gmail.com>
2026-05-09 22:03:38 -0300

    deixei tudo comentado, agora vou entregar pro chat

--- calculadora-de-loja.py
+++ calculadora-de-loja.py
@@ -8,27 +8,26 @@ quantidade_comprada = 4
 valor_total = preço_do_produto * quantidade_comprada
 valor_com_desconto = valor_total * 0.10 
 
-print("quantia:" + str(quantidade_comprada))
-print("preço do produto:" + str(preço_do_produto))
-print("valor total:" + str(valor_total))
-print("valor com desconto de:" + str(valor_com_desconto))
+print("produto: " + nome_produto)
+print("quantia: " + str(quantidade_comprada))
+print("preço: " + str(preço_do_produto))
+print("total: " + str(valor_total))
+print("valor com desconto de: " + str(valor_com_desconto))
 print("valor final:" + str(valor_total - valor_com_desconto))
 
-senha = 1538
-input(print("insira a senha para finalizar a compra: " )), int("senha")
-if print("digite a senha") == 1538:
-    print("compra finalizada com sucesso!")
-else:    print("senha incorreta, tente novamente.")
+#senha = 1538
+##input(print("insira a senha para finalizar a compra: " (print("digite a senha: "))))
+#if senha == 1538:
+ # print("compra finalizada com sucesso!")
+#else:    print("senha incorreta, tente novamente.")
 
-input(print("vamos tentar denovo, certo: y/n "))
-resposta = input()
+#input(print("vamos tentar denovo, certo: y/n "))
+#resposta = input(print("digite sua senha: "))
 
-if resposta == "y":
-    senha = 1538
-    input(print("insira a senha para finalizar a compra: "))
+#if resposta == "y":
+ #   senha = 1538
+  #  input(print("insira a senha para finalizar a compra: "))
 
-else:    print("compra cancelada, obrigado por visitar nossa loja!")
-
-if senha == 1538:
-    print("tente novamente!")
-else:    print("senha incorreta, tente novamente.")
+   # else:    print("compra cancelada, obrigado por visitar nossa loja!")
+#elif resposta == "n":
+#print("compra cancelada, obrigado por visitar nossa loja!")
